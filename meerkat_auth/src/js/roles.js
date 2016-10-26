b01016374c226507d6f25982f5ef0d90d23cf07e
mrjb <jonathan.s.berry@googlemail.com>
2016-10-26 13:43:30 +0100

    Added role descriptions to the access page.

--- meerkat_auth/src/js/roles.js
+++ meerkat_auth/src/js/roles.js
@@ -60,8 +60,64 @@ function drawVis(country){
                 }
             }
         };
+
         var network = new vis.Network(container,graph, options);
 
+        //The click event gives the id of the node selected in a params object.
+        network.on("click", function (params) {
+
+            //Conveniently the id is just the index of the level in the data object.
+            var selected = data[params.nodes[0]];
+            var url = root + '/en/roles/get_all_access/' + selected.country + '/' + selected.role;
+            
+            console.log( selected );
+            console.log( selected.description.trim() || "(No Description)" );
+
+            //Get the selected access levels complete access list from the server.
+            $.getJSON( url, function(all_access){
+
+                //Get the complete role objects for the specified role's access list.
+                var roles = [];
+                all_access.access.map( function( level ){
+                    if( level != selected.role ) roles.push( getObj( data, 'role', level )[0] );
+                });
+
+                //Now we have all the data, we can start displaying it. 
+                var html = "<table class='selected col-xs-12 col-md-8 col-md-offset-2'>" +
+                           "<tbody class='selected-level'>"+
+                           "<tr><td colspan=2><label>Selected access level:</label></td></tr>" +
+                           "<tr class='level'><td class='level__title col-xs-6 col-sm-3 col-md-2'><div>" +
+                           caps(selected.role) + "</div></td>" + 
+                           "<td class='level__description no-pad col-xs-6 col-sm-9 col-md-10'><div>" +
+                           (selected.description.trim() || "(No Description)") + "</div></td></tr></tbody>";
+
+                if( roles.length > 0 ){
+                    html += "<tr><td colspan=2><label>Also inherits access levels:</label></td></tr>" +
+                            "<tbody class='inherits'>";
+                    for( var r in roles ){
+                        html += "<tr class='level'><td class='level__title col-xs-6 col-sm-3 col-md-2'><div>" +
+                                caps(roles[r].role) + "</div></td>" + 
+                                "<td class='level__description no-pad col-xs-6 col-sm-9 col-md-10'><div>" +
+                                (roles[r].description.trim() || "(No Description)") + 
+                                "</div></td></tr>";                    
+                    }
+                }else{
+                    html += "<tr><td colspan=2><label>Doesn't inherit any other access.</label></td></tr>" +
+                            "<tbody class='inherits'>";  
+                }
+                html += "</tbody></table>";
+
+                //Draw!
+                $('.selection-details').html(html);
+
+            });
+        });
+    });
+}
+
+function getObj( array, key, value ){
+    return array.filter( function( element ) {
+        return element[key] == value;
     });
 }
 

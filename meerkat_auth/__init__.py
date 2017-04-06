cc5b2b27e28ba4398e1d9f820ac076d827acd092
mrjb <jonathan.s.berry@googlemail.com>
2017-04-06 11:15:20 +0100

    Moved config into package.

--- meerkat_auth/__init__.py
+++ meerkat_auth/__init__.py
@@ -9,7 +9,7 @@ from flask.ext.babel import Babel
 # Create the Flask app
 app = Flask(__name__)
 babel = Babel(app)
-app.config.from_object('config.Production')
+app.config.from_object('meerkat_auth.config.Production')
 app.config.from_envvar('MEERKAT_AUTH_SETTINGS')
 
 from meerkat_auth.views.users import users

12e985b2d17cfc7773f99b81216d76ffd175440e
mrjb <jonathan.s.berry@googlemail.com>
2017-04-11 12:26:06 +0100

    Fix auth requirements versions.

--- meerkat_auth/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask.ext.babel import Babel
-
-# Create the Flask app
-app = Flask(__name__)
-babel = Babel(app)
-app.config.from_object('meerkat_auth.config.Production')
-app.config.from_envvar('MEERKAT_AUTH_SETTINGS')

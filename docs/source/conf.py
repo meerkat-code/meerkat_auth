bb318106b38209bd20242469486a6fb956eb0a60
Jonathan Berry <jonathan.s.berry@googlemail.com>
2018-02-16 08:22:33 +0000

    PLaying with RTD builds.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,9 @@ class Mock(MagicMock):
 def mock_modules():
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
+
+mock_modules()
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
